98e0711b05bc289c05763e637da34a3d4d110bf7
M.K <miika.kallasoja@gmail.com>
2021-04-18 13:12:20 +0300

    Bug fix related to moving meals from one slot to another

--- client/src/components/WeeklyCalendar.js
+++ client/src/components/WeeklyCalendar.js
@@ -52,7 +52,8 @@ const [meal, setMeal] = useState("");
     const movedMeal = meals.find((meal) => meal.id == mealObj.id);
     
     // update meal object with new day and meal type details
-    const target = JSON.parse(e.target.id);
+    const target = JSON.parse(e.currentTarget.id);
+    console.log(target)
     if (meals.some(meal => meal.day === target.day && meal.type === target.meal.value))
       return
 
