7295072f81ee2373469c40e9fef0368b421405f1
M.K <miika.kallasoja@gmail.com>
2021-04-25 09:13:14 +0300

    auth service

---
+++ client/src/services/Auth.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const baseUrl = 'https://localhost:3000/api/auth'
+
+const register = async (newUser) => {
+    const req = axios.post(`${baseUrl}/register`, newUser)
+    return req.then(response => response.data)
+}
+
+const login = async (user) => {
+    const req = axios.post(`${baseUrl}/login`, user)
+    return req.then(response => response.data)
+}
+
+export default { register, login }
