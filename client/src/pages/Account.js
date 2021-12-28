6016ffaa79557dccc2568e89bab234b17de9d95c
M.K <miika.kallasoja@gmail.com>
2021-12-28 17:19:00 +0200

    acount form fields wider

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -85,7 +85,6 @@ const loggedInUser = JSON.parse(window.localStorage.getItem('loggedInUser'))
 useEffect(() => {    
     if (loggedInUser && loggedInUser.id) {
     Users.get(loggedInUser.id).then(data => {
-        console.log(data);
         
         const fields = {
             'first_name': {'value': data.first_name},
@@ -111,26 +110,26 @@ if (show) {
         <div className='form-wrapper'>
         <form>
         <div id="first_name">
-        <label><b>Name:</b></label> <input name='first_name' onChange={(event) => handleChange(event)} value={first_name ? first_name.value : null} disabled/>
+        <label><b>Name:</b></label> <input className='account' name='first_name' onChange={(event) => handleChange(event)} value={first_name ? first_name.value : null} disabled/>
         {!editMode.first_name ? <span className='edit-save-field' id='edit_firstName' onClick={(event) => enableEditing(event)} >Edit</span> : null}
         {editMode.first_name ? <span className='edit-save-field' id='save_firstName' onClick={(event) => saveChanges(event)} >Save</span> : null}
         </div>
                 <div id="username">
 
-        <label><b>Username:</b></label> <input name='username' value={username ? username.value : null} disabled/>
+        <label><b>Username:</b></label> <input className="account" name='username' value={username ? username.value : null} disabled/>
         </div>
          <div id="password">
-        <label><b>Password:</b></label> <input name='password' type='password' onChange={(event) => handleChange(event)} value={password ? password.value : null} disabled/>
+        <label><b>Password:</b></label> <input className="account" name='password' type='password' onChange={(event) => handleChange(event)} value={password ? password.value : null} disabled/>
          {!editMode.password ? <span className='edit-save-field' id='edit_password' onClick={(event) => enableEditing(event)} >Edit</span> : null}
         {editMode.password ? <span className='edit-save-field' id='save_password' onClick={(event) => saveChanges(event)} >Save</span> : null}
         </div>
         <div id="email">
-        <label><b>Email:</b></label> <input name='email' onChange={(event) => handleChange(event)} value={email ? email.value : null} disabled/>
+        <label><b>Email:</b></label> <input className="account" name='email' onChange={(event) => handleChange(event)} value={email ? email.value : null} disabled/>
          {!editMode.email ? <span className='edit-save-field' id='edit_email' onClick={(event) => enableEditing(event)} >Edit</span> : null}
         {editMode.email ? <span className='edit-save-field' id='save_email' onClick={(event) => saveChanges(event)} >Save</span> : null}
        </div>
         <div id="email">
-        <label><b>Household name:</b></label> <input name='household_name' onChange={(event) => handleChange(event)} value={household_name ? household_name.value : null} disabled/>
+        <label><b>Household name:</b></label> <input className="account" name='household_name' onChange={(event) => handleChange(event)} value={household_name ? household_name.value : null} disabled/>
        </div>
         </form>
         </div>
