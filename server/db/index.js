36233378e29405ecb21292983beedf3f1ead1cea
M.K <miika.kallasoja@gmail.com>
2022-03-06 08:54:45 +0200

    environment variables

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
 const { Pool, Client } = require('pg')
 
 const connectInfo = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'mealplanner',
-    password: 'G0r3b0nm0',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 }
 
 const pool = new Pool(connectInfo);
