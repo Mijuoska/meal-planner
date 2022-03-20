13a13cb53dbd9ac95c2681570abbc817b776058b
M.K <miika.kallasoja@gmail.com>
2022-03-20 08:45:26 +0200

    DB ssl connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,31 @@
 const { Pool, Client } = require('pg')
+const fs = require('fs')
 
-const connectInfo = {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
+const configDev = {
+     user: process.env.DB_USER,
+     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+
+    ssl: {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync(process.env.DB_SERVER_CERTIFICATE).toString(),
+        key: fs.readFileSync(process.env.DB_CLIENT_KEY).toString(),
+        cert: fs.readFileSync(process.env.DB_CLIENT_CERTIFICATE).toString()
+    }
 }
 
-const pool = new Pool(connectInfo);
+const configProd = {
+    connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+}
+
+const pool = process.env.NODE_ENV == 'dev' ? new Pool(configDev) : new Pool(configProd)
+ 
+
 
 
 module.exports = {
